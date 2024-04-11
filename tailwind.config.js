e44533888255c041974759ae7f3b4e9d036e547d
Manish Sindur <manishsindur@gmail.com>
2024-04-11 17:54:24 +0530

    Add initial Tailwind CSS configuration with custom fonts

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["*"],
+	theme: {
+		extend: {
+			fontFamily: {
+				Young_Serif: ["Young Serif", "serif"],
+				Outfit_sansserif: ["Outfit", "sans - serif"],
+			},
+		},
+	},
+	plugins: [],
+};
